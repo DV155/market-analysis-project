8a6ca2301843ded2384257c42182d7cce116857e
DV155 <dan0024175@gmail.com>
2026-01-23 19:04:21 +0100

    Added 2025 cpi calc

--- mainProject.py
+++ mainProject.py
@@ -9,8 +9,13 @@ desFactor = input("What basket component do you want?")
 desYear = input("What year do you want?")
 validFactors = ["cpi_overall", "food_beverages", "alcohol_tobacco", "clothing_footwear", "housing_utilities", "furnishings_household",	"health", "transport", "communication", "recreation_culture", "education", "restaurants_hotels", "miscellaneous"]
 if desFactor in validFactors:
-    cur.execute(f"SELECT {desFactor}, year FROM available_data WHERE year = (?)", (desYear,))
+    cur.execute(f"SELECT {desFactor} FROM available_data WHERE year = (?)", (desYear,))
     result = cur.fetchone()
-    print(result)
+    print(result[0])
 else:
     print("Invalid querry")
+cur.execute("SELECT cpi_overall FROM available_data WHERE year = 2025")
+currentCPI = cur.fetchone()
+print("2025 Austrian CPI is", currentCPI[0])
+maxValue = max(result[1:])
+print("Highest CPI contributor in 2025 is at", maxValue)
