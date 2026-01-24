cc0c1e3ec78b98c2599adf0caa2a286b392b0663
DV155 <dan0024175@gmail.com>
2026-01-24 17:16:00 +0100

    Moved input clause below main stats, expanded how current max contributor is handled

--- mainProject.py
+++ mainProject.py
@@ -5,6 +5,15 @@ import matplotlib.pyplot as plt
 conn = sqlite3.connect("austrian_cpi.db")
 cur = conn.cursor()
 
+currentCPI = cur.fetchone()
+print("2025 Austrian CPI is", currentCPI[0])
+cur.execute("SELECT * from available_data WHERE year = 2025")
+maxValue = cur.fetchone()
+neededValue = max(maxValue[2:])
+column_names = [desc[0] for desc in cur.description]
+max_index = maxValue.index(neededValue)  # Skip year column
+max_column = column_names[max_index]
+print("Biggest contributor in 2025 is " + max_column + " at " +  str(neededValue))
 desFactor = input("What basket component do you want?")
 desYear = input("What year do you want?")
 validFactors = ["cpi_overall", "food_beverages", "alcohol_tobacco", "clothing_footwear", "housing_utilities", "furnishings_household",	"health", "transport", "communication", "recreation_culture", "education", "restaurants_hotels", "miscellaneous"]
@@ -15,9 +24,3 @@ if desFactor in validFactors:
 else:
     print("Invalid querry")
 cur.execute("SELECT cpi_overall FROM available_data WHERE year = 2025")
-currentCPI = cur.fetchone()
-print("2025 Austrian CPI is", currentCPI[0])
-cur.execute("SELECT * from available_data WHERE year = 2025")
-maxValue = cur.fetchone()
-neededValue = max(maxValue[1:])
-print("Biggest contributor in 2025 is at", neededValue)
