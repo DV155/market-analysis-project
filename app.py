aa156c5dffaa582649e944036825b59a1ed8a878
DV155 <dan0024175@gmail.com>
2026-01-29 15:13:45 +0100

    Modified app, created css and html basics

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+import sqlite3
+import math
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///austrian_cpi.db'
 
+conn = sqlite3.connect("austrian_cpi.db")
+cur = conn.cursor()
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
