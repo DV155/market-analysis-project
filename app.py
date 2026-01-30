bda439471a7bd61bea581099e090bde8e4a2403e
DV155 <dan0024175@gmail.com>
2026-01-30 19:26:09 +0100

    Added MVP of graph in app.py, experimented in mainProject file

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 import math
-import matplotlib.pyplot as plt
+#import plotly.express as px
+import plotly.graph_objects as go
+from waitress import serve
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///austrian_cpi.db'
-
-conn = sqlite3.connect("austrian_cpi.db")
-cur = conn.cursor()
+def get_db_connection():
+    conn = sqlite3.connect('austrian_cpi.db')
+    return conn
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT year, cpi_overall FROM available_data")
+    data = cur.fetchall()
+    conn.close()
+
+    years = [row[0] for row in data]
+    cpi = [row[1] for row in data]
+
+    fig = go.Figure(data=go.Scatter(x=years, y=cpi, mode='lines+markers'))
+    #fig.update_layout(title='Austrian CPI', xaxis_title='', yaxis_title='')
+    
+    graph_html = fig.to_html(full_html=False)
+    
+    return render_template('index.html', graph=graph_html)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app)
