3c7067a53fdbf0863d77a468e71461f15f460fee
DV155 <dan0024175@gmail.com>
2026-02-08 09:32:51 +0100

    Expanded functionality to give last available CPI, added html links and an about page

--- app.py
+++ app.py
@@ -16,17 +16,22 @@ def index():
     cur = conn.cursor()
     cur.execute("SELECT year, cpi_overall FROM available_data")
     data = cur.fetchall()
-    conn.close()
 
     years = [row[0] for row in data]
     cpi = [row[1] for row in data]
 
     fig = go.Figure(data=go.Scatter(x=years, y=cpi, mode='lines+markers'))
     config = {'staticPlot': True}
+    cur.execute("SELECT cpi_overall FROM available_data WHERE year = 2025")
+    currentCPI = cur.fetchone()
+    cur.execute("SELECT cpi_overall FROM available_data WHERE year = 2024")
+    lastYearCPI = cur.fetchone()
+    inflationCalc = round(((currentCPI[0] - lastYearCPI[0]) / lastYearCPI[0] ) * 100, 2)
+    conn.close()
     
     graph_html = fig.to_html(full_html=False)
     
-    return render_template('index.html', graph=graph_html)
+    return render_template('index.html', graph=graph_html, inflationValue=inflationCalc)
 
 if __name__ == "__main__":
     serve(app)
