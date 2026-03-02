e492b8efdc89c89ff4e56f51b04aaadcb5264d68
DV155 <dan0024175@gmail.com>
2026-03-02 16:33:54 +0100

    added wip of german-language pages

--- app.py
+++ app.py
@@ -57,9 +57,60 @@ def index():
     
     return render_template('index.html', graph=graph_html, lastYear=lastYear, currentYear=currentYear, inflationValue=inflationCalc, neededValue=neededValue, clean_name=clean_name, foodDif=foodDif, edDif=edDif, healthDif=healthDif)
 
+@app.route('/de')
+def indexDE():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT year, cpi_overall FROM available_data")
+    data = cur.fetchall()
+
+    years = [row[0] for row in data]
+    cpi = [row[1] for row in data]
+    dateToday = date.today()
+    currentYearCalc = dateToday.year
+    currentYear = str(currentYearCalc - 1)
+    lastYear = str(currentYearCalc - 2)
+
+    fig = go.Figure(data=go.Scatter(x=years, y=cpi, mode='lines+markers', line=dict(color='#4ade80'), marker=dict(color='#4ade80')))
+    config = {'staticPlot': True}
+    fig.update_layout(plot_bgcolor='#2d2d2d', paper_bgcolor='#1e1e1e', font=dict(color='#e0e0e0'),xaxis=dict(gridcolor='#444444',tickfont=dict(color='white')),yaxis=dict(gridcolor='#444444',tickfont=dict(color='white')))
+    cur.execute(f"SELECT cpi_overall FROM available_data WHERE year = {currentYear}") #Last year inflaton
+    currentCPI = cur.fetchone()
+    cur.execute(f"SELECT cpi_overall FROM available_data WHERE year = {lastYear}")
+    lastYearCPI = cur.fetchone()
+    inflationCalc = round(((currentCPI[0] - lastYearCPI[0]) / lastYearCPI[0] ) * 100, 2)
+
+    cur.execute(f"SELECT * from available_data WHERE year = {currentYear}") #Max contributor
+    maxValue = cur.fetchone()
+    neededValue = max(maxValue[2:])
+    column_names = [desc[0] for desc in cur.description]
+    max_index = maxValue.index(neededValue) 
+    max_column = column_names[max_index]
+    clean_name = max_column.title().replace('_', ' and ')
+
+    cur.execute(f"SELECT food_beverages from available_data WHERE year IN ({lastYear}, {currentYear}) ORDER BY year") #The big 3
+    foodData = cur.fetchall()
+    foodDif = round(((foodData[1][0] - foodData[0][0]) / foodData[0][0] ) * 100, 2)
+    cur.execute(f"SELECT education from available_data WHERE year IN ({lastYear}, {currentYear}) ORDER BY year")
+    edData = cur.fetchall()
+    edDif = round(((edData[1][0] - edData[0][0]) / edData[0][0] ) * 100, 2)
+    cur.execute(f"SELECT health from available_data WHERE year IN ({lastYear}, {currentYear}) ORDER BY year")
+    healthData = cur.fetchall()
+    healthDif = round(((healthData[1][0] - healthData[0][0]) / healthData[0][0] ) * 100, 2)
+    conn.close()
+    
+    graph_html = fig.to_html(full_html=False)
+    
+    return render_template('indexDE.html', graph=graph_html, lastYear=lastYear, currentYear=currentYear, inflationValue=inflationCalc, neededValue=neededValue, clean_name=clean_name, foodDif=foodDif, edDif=edDif, healthDif=healthDif)
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/aboutDE')
+def aboutDE():
+    return render_template('aboutDE.html')
 
 @app.route('/calc')
 def calc():
@@ -69,6 +120,15 @@ def calc():
 
     conn.close()
     return render_template('calculator.html')
+
+@app.route('/calcDE')
+def calcDE():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT year, cpi_overall FROM available_data")
+
+    conn.close()
+    return render_template('calculatorDE.html')
 
 if __name__ == "__main__":
     serve(app)
