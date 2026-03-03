2ea3258b3be044b1b57ffce3fb3ead75d5585511
DV155 <dan0024175@gmail.com>
2026-03-03 14:11:29 +0100

    used js to implement moving counters on eng-lang index page

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,10 @@
-const countUp = new countUp();
-countUp.start()
+const value1 = parseFloat(document.getElementById('foodCounter').dataset.value);
+const food = new countUp.CountUp('foodCounter', value1, { decimalPlaces: 2 });
+const value2 = parseFloat(document.getElementById('edCounter').dataset.value);
+const edu = new countUp.CountUp('edCounter', value2, { decimalPlaces: 2 });
+const value3 = parseFloat(document.getElementById('hpCounter').dataset.value);
+const health = new countUp.CountUp('hpCounter', value3, { decimalPlaces: 2 });
+factorList = [food, edu, health];
+for (factor of factorList) {
+    factor.start();
+}
