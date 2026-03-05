884fdd771c4df6761b577aa11cda3254adf0ebd3
DV155 <dan0024175@gmail.com>
2026-03-05 13:02:42 +0100

    made a better counter for both en and de page

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,8 @@
-const value1 = parseFloat(document.getElementById('foodCounter').dataset.value);
-const food = new countUp.CountUp('foodCounter', value1, { decimalPlaces: 2 });
-const value2 = parseFloat(document.getElementById('edCounter').dataset.value);
-const edu = new countUp.CountUp('edCounter', value2, { decimalPlaces: 2 });
-const value3 = parseFloat(document.getElementById('hpCounter').dataset.value);
-const health = new countUp.CountUp('hpCounter', value3, { decimalPlaces: 2 });
-factorList = [food, edu, health];
-for (factor of factorList) {
-    factor.start();
+const factorList = [];
+const elementList = document.getElementsByClassName("toBeLookedThrough");
+for (element of elementList) {
+    const values = parseFloat(element.dataset.value);
+    const counter = new countUp.CountUp(element.id, values, { decimalPlaces: 2 });
+    counter.start();
+    
 }
